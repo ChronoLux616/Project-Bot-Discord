b36bc00ddee64b503d83cdd032116e8e48ca927e
Chrono Lux <m.sanma123@gmail.com>
2021-03-08 22:28:09 -0600

    delete

--- cogs/test.py
+++ cogs/test.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import discord
 
 class Test(commands.Cog):
     def __init__(self, bot):
@@ -7,6 +8,10 @@ class Test(commands.Cog):
     @commands.command()
     async def hello(self, ctx, *args):
         await ctx.send(" - ".join(args))
+    
+    @commands.command()
+    async def joined(self, ctx, *, member: discord.Member):
+        await ctx.send('{0} joined on {0.joined_at}'.format(member))
 
 def setup(bot):
     bot.add_cog(Test(bot))
