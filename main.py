6b3a8fd73de1eeb1192aa8cd0a0c2adfaad2677f
ChronoLux616 <m.sanma123@gmail.com>
2022-12-10 20:02:03 -0600

    rechanges

--- main.py
+++ main.py
@@ -4,19 +4,4 @@ from settings import *
 
 bot = commands.Bot(command_prefix="+")
 
-# agrega los arhivos de carpeta cogs necesarios
-for filename in os.listdir("./cogs"):
-    if filename.endswith(".py") and filename != "__init__.py":
-        bot.load_extension(f'cogs.{filename[:-3]}')
-
-# agrega los arhivos de carpeta tools necesarios
-for filename in os.listdir("./tools"):
-    if filename.endswith(".py") and filename != "__init__.py":
-        bot.load_extension(f'tools.{filename[:-3]}')
-
-# agrega los arhivos de carpeta info necesarios
-for filename in os.listdir("./info"):
-    if filename.endswith(".py") and filename != "__init__.py":
-        bot.load_extension(f'info.{filename[:-3]}')
-
-bot.run(DISCORD_BOT_TOKEN)
+bot.run('TOKEN')
