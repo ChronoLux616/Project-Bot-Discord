73e2f42ce19092c81083cee9b9012f7ce3ddc708
ChronoLux616 <m.sanma123@gmail.com>
2022-12-12 15:57:50 -0600

    delete

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-import os
+import settings
+import discord
 from discord.ext import commands
-from settings import *
 
-bot = commands.Bot(command_prefix="+")
+logger = settings.logging.getLogger("bot")
 
-bot.run('TOKEN')
+def run():
+    intents = discord.Intents.default()
+    intents.message_content = True
+    
+    bot = commands.Bot(command_prefix="+", intents=intents)
+    
+    @bot.event
+    async def on_ready():
+        logger.info(f"User: {bot.user} (ID: {bot.user.id}")
+    
+    @bot.command()
+    async def ping(ctx):
+        await ctx.send("pong")
+        
+    bot.run(settings.DISCORD_API_SECRET, root_logger=True)
+    
+if __name__ == "__main__":
+    run()
